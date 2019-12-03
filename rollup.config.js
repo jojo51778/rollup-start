2f0452b6906472f2a407bb48978ec6f40e17add3
echo <623025156@qq.com>
2019-12-03 15:57:00 +0800

    👌 add external

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import json from 'rollup-plugin-json'
+import resolve from 'rollup-plugin-node-resolve'
 
 export default {
   input: 'src/main.js',
@@ -6,5 +6,12 @@ export default {
     file: 'bundle.js',
     format: 'cjs'
   },
-  plugins: [ json() ]
+  plugins: [resolve({
+    // 将自定义选项传递给解析插件
+    customResolveOptions: {
+      moduleDirectory: 'node_modules'
+    }
+  })],
+  // 外部模块
+  external: ['lodash']
 }
